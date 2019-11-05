d92921905b3caf121dafc796701bdba272907e35
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2019-11-05 06:27:29 -0600

    Update NearestPlayerCompass.php

--- src/anirudh246/NearestPlayerCompass/NearestPlayerCompass.php
+++ src/anirudh246/NearestPlayerCompass/NearestPlayerCompass.php
@@ -1,1 +1,68 @@
+<?php
+namespace anirudh246\NearestPlayerCompass;
 
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerItemHeldEvent;
+use pocketmine\item\ItemIds;
+use pocketmine\level\Position;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\SetSpawnPositionPacket;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class NearestPlayerCompass extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        @mkdir($this->getDataFolder(), 0744, true);
+        $this->saveResource('config.yml', false);
+        $this->config = new Config($this->getDataFolder().'config.yml', Config::YAML);
+    }
+
+    public function onItemHeld(PlayerItemHeldEvent $event){
+        $player = $event->getPlayer();
+        if($event->getItem()->getId() === ItemIds::COMPASS){
+
+                $setNeedle = (bool) $this->config->get('set-needle-to-nearest');
+                $nearPlayer = $this->calculateNearestPlayer($player);
+                if($nearPlayer instanceof Player){
+                    $myVector = $player->asVector3();
+                    $nearVector = $nearPlayer->asVector3();
+                    $message = \str_replace(['@pn', '@dn', '@tn', '@d'] ,[$nearPlayer->getName(), $nearPlayer->getDisplayName(), $nearPlayer->getNameTag(), (int) $myVector->distance($nearVector)], $this->config->get('message-found-nearest-player'));
+                    if($setNeedle) $this->setSpawnPositionPacket($player, $nearVector);
+                }else{
+                    $player->sendMessage($this->config->get('message-no-nearest-player'));
+                }
+            }
+        }
+    }
+}
+    }
+
+    private function setSpawnPositionPacket(Player $player, Vector3 $pos) : void{
+        $pk = new SetSpawnPositionPacket();
+        $pk->spawnType = SetSpawnPositionPacket::TYPE_WORLD_SPAWN;
+        $pk->x = (int) $pos->getX();
+        $pk->y = (int) $pos->getY();
+        $pk->z = (int) $pos->getZ();
+        $pk->spawnForced = false;
+        $player->dataPacket($pk);
+    }
+
+    private function calculateNearestPlayer(Player $player) : ?Player{
+        $closest = null;
+        if($player instanceof Position){
+            $lastSquare = -1;
+            $onLevelPlayer = $player->getLevel()->getPlayers();
+            unset($onLevelPlayer[array_search($player, $onLevelPlayer)]);
+            foreach($onLevelPlayer as $p){
+                $square = $player->distanceSquared($p);
+                if($lastSquare === -1 || $lastSquare > $square){
+                    $closest = $p;
+                    $lastSquare = $square;
+                }
+            }
+        }
+        return $closest;
+    }
+}
