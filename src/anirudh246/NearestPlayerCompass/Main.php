5c28ff8d1c88d8e8267388bf66bdedd517d7af31
anirudh246 <51390305+anirudh246@users.noreply.github.com>
2019-12-25 14:05:18 -0600

    Update Main.php

--- src/anirudh246/NearestPlayerCompass/Main.php
+++ src/anirudh246/NearestPlayerCompass/Main.php
@@ -24,8 +24,7 @@ class Main extends PluginBase implements Listener
         @mkdir($this->getDataFolder(), 0744, true);
         $this->saveResource('config.yml', false);
         $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
-        TaskScheduler::scheduleRepeatingTask(\pocketmine\task\Task $task,int $period)}
-        $this->getScheduler()->scheduleRepeatingTask(new CompassTask($this), (int)$this->getConfig()->get("update-interval") * 20);
+        $this->getScheduler()->scheduleRepeatingTask(new CompassTask, 20);
 
 
         }
