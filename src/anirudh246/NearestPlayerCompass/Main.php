e54f10612668feddc6c961fba3ec30649e89fa52
xXNiceYT <23722462+xXNiceYT@users.noreply.github.com>
2019-12-28 18:57:35 -0500

    Added some changes but not tested

--- src/anirudh246/NearestPlayerCompass/Main.php
+++ src/anirudh246/NearestPlayerCompass/Main.php
@@ -17,17 +17,86 @@ use anirudh246\NearestPlayerCompass\Main;
 
 
 class Main extends PluginBase implements Listener{
-    public function onEnable()
-    {
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        @mkdir($this->getDataFolder(), 0744, true);
-        $this->saveResource('config.yml', false);
-        $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
-        $this->getScheduler()->scheduleRepeatingTask(new CompassTask, 20);
-
-
+    
+    private static $instance = null;
+    
+    public function onLoad(): void{
+        self::$instance = $this;
     }
-
+    
+    public function onEnable(): void{
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getScheduler()->scheduleRepeatingTask(new CompassTask(), 15);
+    }
+    
+    public static function getInstance(): self{
+        return self::$instance;
+    }
+    
+    public function onItemHeld(PlayerItemHeldEvent $event){
+        $player = $event->getPlayer();
+        if($event->getItem()->getId() === ItemIds::COMPASS){
+            $onlyOperator = (bool) $this->config->get('apply-only-permitted-player');
+            if(($onlyOperator && $player->hasPermission('nearestplayercompass.allow.permission')) || $onlyOperator === false){
+                $this->sendCalculatedMessage($player);
+            }
+        }
+    }
+ 
+    public function sendEachType(Player $player, string $message): void{
+        switch(strtolower($this->config->get('sending-message-type'))){
+            case 'tip':
+                $player->sendTip($message);
+                break;
+            case 'popup':
+                $player->sendPopup($message);
+                break;
+            default:
+                $player->sendMessage($message);
+                break;
+        }
+    }
+    
+    public function setSpawnPositionPacket(Player $player, Vector3 $pos): void{
+        $pk = new SetSpawnPositionPacket();
+        $pk->spawnType = SetSpawnPositionPacket::TYPE_WORLD_SPAWN;
+        $pk->x = (int) $pos->getX();
+        $pk->y = (int) $pos->getY();
+        $pk->z = (int) $pos->getZ();
+        $pk->spawnForced = false;
+        $player->dataPacket($pk);
+    }
+    
+    public function calculateNearestPlayer(Player $player): ?Player{
+        $closest = null;
+        if($player instanceof Position){
+            $lastSquare = -1;
+            $onLevelPlayer = $player->getLevel()->getPlayers();
+            unset($onLevelPlayer[array_search($player, $onLevelPlayer)]);
+            foreach($onLevelPlayer as $p){
+                $square = $player->distanceSquared($p);
+                if($lastSquare === -1 || $lastSquare > $square){
+                    $closest = $p;
+                    $lastSquare = $square;
+                }
+            }
+        }
+        return $closest;
+    }
+    
+    public function sendCalculatedMessage(Player $player): void{
+        $nearPlayer = $this->calculateNearestPlayer($player);
+        $setNeedle = (bool) $this->config->get('set-needle-to-nearest');
+        if($nearPlayer instanceof Player){
+            $myVector = $player->asVector3();
+            $nearVector = $nearPlayer->asVector3();
+            $message = \str_replace(['@pn', '@dn', '@tn', '@d'] ,[$nearPlayer->getName(), $nearPlayer->getDisplayName(), $nearPlayer->getNameTag(), (int) $myVector->distance($nearVector)], $this->config->get('message-found-nearest-player'));
+            $this->sendEachType($player, $message);
+            if($setNeedle) $this->setSpawnPositionPacket($player, $nearVector);
+        }else{
+            $player->sendMessage($this->config->get('message-no-nearest-player'));
+        }
+    }
 }
-
-
